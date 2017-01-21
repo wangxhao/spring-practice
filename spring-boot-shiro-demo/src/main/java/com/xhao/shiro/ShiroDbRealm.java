4c7c3927a18f48e66f49f9b8bd0452f1adda0e5e
WangXianHao <i.wang.xhao@gmail.com>
2017-01-21 15:38:36 +0800

    add 自定义操作员认证

---
+++ spring-boot-shiro-demo/src/main/java/com/xhao/shiro/ShiroDbRealm.java
@@ -0,0 +1,29 @@
+package com.xhao.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by xhaow on 2017/1/21.
+ */
+public class ShiroDbRealm extends AuthorizingRealm {
+
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo();
+        return null;
+    }
+}
